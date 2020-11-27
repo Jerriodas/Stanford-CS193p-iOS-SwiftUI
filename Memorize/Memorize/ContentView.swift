86806be8977817a7037c01fefa33528cf13b7b21
Jerriodas <zeus12gerardo@gmail.com>
2020-11-26 23:05:47 -0600

    Added two sides CardView

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack(content: {
-            RoundedRectangle(cornerRadius: 10.0).stroke().foregroundColor(.orange)
-            Text("👻")
-        })
+        return HStack {
+            ForEach(0 ..< 4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+            .padding()
+            .foregroundColor(.orange)
+            .font(Font.largeTitle)
     }
 }
 
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
 
 
 
