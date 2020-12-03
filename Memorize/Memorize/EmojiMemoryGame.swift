e0308008e0827b313e8b3e4871f228b3ad81937b
Jerriodas <zeus12gerardo@gmail.com>
2020-12-03 13:52:02 -0600

    Link view with model

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String>
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃"]
+        return MemoryGame(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
     
     //MARK: -Access to the model
     var cards: Array<MemoryGame<String>.Card> {
--- Memorize/Memorize/MemoryGame.swift
+++ Memorize/Memorize/MemoryGame.swift
@@ -10,10 +10,13 @@ import Foundation
 struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
-    init(numberOfPairsOfCards: Int) {
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = Array<Card>()
         for pairIndex in 0 ..< numberOfPairsOfCards {
-         //   cards.append()
+            let content = cardContentFactory(pairIndex)
+            
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
         }
     }
     
@@ -21,9 +24,10 @@ struct MemoryGame<CardContent> {
         print("Card chosen: \(card)")
     }
     
-    struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+    struct Card: Identifiable {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
+        var id: Int
     }
 }
